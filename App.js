3c9a7e0ddc63e27d7547019cb58ce7c616988aa8
Wajeeh <wajeeh.zantout@gmail.com>
2018-07-18 14:00:09 +0300

    chore: add apollo config

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
 // @flow
 
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text } from 'react-native';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: 'http://localhost:4000' }),
+  cache: new InMemoryCache(),
+});
 
 const App = () => (
-  <View>
+  <ApolloProvider client={client}>
     <Text>
 React Native App
     </Text>
-  </View>
+  </ApolloProvider>
 );
 
 export default App;
